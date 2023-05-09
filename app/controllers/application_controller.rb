182d4584819604b72f9681df5c70f7f9a0add59e
Moxvallix <moxvallix@gmail.com>
2023-05-09 17:38:59 +0930

    chore: add database connection error page, for when the database is warming up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,17 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
 
   before_action :configure_devise_parameters, if: :devise_controller?
+  
+  rescue_from ActiveRecord::ConnectionNotEstablished, with: :db_connection_error
 
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[featured_skin_id])
+  end
+
+  def db_connection_error
+    respond_to do |format|
+      format.html { render layout: "plain", template: "errors/connect", status: 202 }
+    end
   end
 end
