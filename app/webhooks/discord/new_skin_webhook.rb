b25189a6386b5018399eabdee99a49c5bec3330a
Moxvallix Mox <moxvallix@gmail.com>
2023-06-04 22:53:10 +0930

    feat(webhooks): implement skin creation webhook

---
+++ app/webhooks/discord/new_skin_webhook.rb
@@ -0,0 +1,22 @@
+class Discord::NewSkinWebhook
+  require "discordrb/webhooks"
+  WEBHOOK_URL = ENV["NEW_SKIN_WEBHOOK"]
+  
+  class << self
+    include Routing
+    def client
+      @@client ||= Discordrb::Webhooks::Client.new(url: WEBHOOK_URL)
+    end
+
+    def send_webhook(skin)
+      raise "Is not a skin" unless skin.is_a? Skin
+      client.execute do |builder|
+        builder.add_embed do |embed|
+          embed.title = "New Skin: **#{skin.name}** by **#{skin.user.name}**."
+          embed.description = "[Link to skin](#{skin_url(skin)})\n#{skin.description}"
+          embed.timestamp = skin.created_at
+        end
+      end
+    end
+  end
+end
