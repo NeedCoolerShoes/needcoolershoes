4f03b7d26ea8a245526076b54bd016390d39200a
Moxvallix <moxvallix@gmail.com>
2023-10-17 12:14:01 +1030

    chore: add captcha to site messages, placeholders for sign in fields

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,7 @@
 class StaticController < ApplicationController
+  CAPTCHA_QUESTION = "What item does a Creeper drop?"
+  CAPTCHA_REGEX = /gun\s*powder/i
+
   prepend_before_action :protect_from_spam, :only => [:send_message]
 
   def editor
@@ -12,6 +15,7 @@ class StaticController < ApplicationController
   end
 
   def contact
+    @question = CAPTCHA_QUESTION
     render layout: "application"
   end
 
@@ -21,7 +25,9 @@ class StaticController < ApplicationController
   def send_message
     send_request_webhook
     respond_to do |format|
-      if Discord::SiteMessageWebhook.send_webhook(**contact_params.to_h.symbolize_keys)
+      if !params[:question].to_s.match?(CAPTCHA_REGEX)
+        format.html { redirect_to root_path, notice: "Message was sent successfully!" }
+      elsif Discord::SiteMessageWebhook.send_webhook(**contact_params.to_h.symbolize_keys)
         format.html { redirect_to root_path, notice: "Message was sent successfully!" }
       else
         format.html { redirect_to root_path, alert: "Error sending message." }
