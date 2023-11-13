03215f1aea6a14819e632eb9451d9a19aa70459c
Moxvallix <moxvallix@gmail.com>
2023-11-13 14:08:17 +1030

    fix: swap left and right leg uvmaps to fix preview images

--- app/models/concerns/skin_transformations.rb
+++ app/models/concerns/skin_transformations.rb
@@ -60,14 +60,14 @@ module SkinTransformations
     {uv: %i[torso_back torso_overlay_back], coordinates: [24, 8]},
     {uv: %i[torso torso_overlay], coordinates: [4, 8]},
     {uv: %i[torso_back torso_overlay_back], coordinates: [24, 8]},
-    {uv: %i[left_leg left_leg_overlay], coordinates: [4, 20]},
-    {uv: %i[left_leg_back left_leg_overlay_back], coordinates: [24, 20]},
-    {uv: %i[right_leg right_leg_overlay], coordinates: [8, 20]},
+    {uv: %i[right_leg right_leg_overlay], coordinates: [4, 20]},
     {uv: %i[right_leg_back right_leg_overlay_back], coordinates: [28, 20]},
-    {uv: %i[left_arm left_arm_overlay], coordinates: [0, 8]},
-    {uv: %i[left_arm_back left_arm_overlay_back], coordinates: [32, 8]},
-    {uv: %i[right_arm right_arm_overlay], coordinates: [12, 8]},
-    {uv: %i[right_arm_back right_arm_overlay_back], coordinates: [20, 8]}
+    {uv: %i[left_leg left_leg_overlay], coordinates: [8, 20]},
+    {uv: %i[left_leg_back left_leg_overlay_back], coordinates: [24, 20]},
+    {uv: %i[right_arm right_arm_overlay], coordinates: [0, 8]},
+    {uv: %i[right_arm_back right_arm_overlay_back], coordinates: [32, 8]},
+    {uv: %i[left_arm left_arm_overlay], coordinates: [12, 8]},
+    {uv: %i[left_arm_back left_arm_overlay_back], coordinates: [20, 8]}
   ]
 
   UV_FRONTBACK_SLIM = [
@@ -77,14 +77,14 @@ module SkinTransformations
     {uv: %i[torso_back torso_overlay_back], coordinates: [24, 8]},
     {uv: %i[torso torso_overlay], coordinates: [4, 8]},
     {uv: %i[torso_back torso_overlay_back], coordinates: [24, 8]},
-    {uv: %i[left_leg left_leg_overlay], coordinates: [4, 20]},
-    {uv: %i[left_leg_back left_leg_overlay_back], coordinates: [24, 20]},
-    {uv: %i[right_leg right_leg_overlay], coordinates: [8, 20]},
+    {uv: %i[right_leg right_leg_overlay], coordinates: [4, 20]},
     {uv: %i[right_leg_back right_leg_overlay_back], coordinates: [28, 20]},
-    {uv: %i[left_arm left_arm_overlay], coordinates: [1, 8]},
-    {uv: %i[left_arm_back left_arm_overlay_back], coordinates: [32, 8]},
-    {uv: %i[right_arm right_arm_overlay], coordinates: [12, 8]},
-    {uv: %i[right_arm_back right_arm_overlay_back], coordinates: [21, 8]}
+    {uv: %i[left_leg left_leg_overlay], coordinates: [8, 20]},
+    {uv: %i[left_leg_back left_leg_overlay_back], coordinates: [24, 20]},
+    {uv: %i[right_arm right_arm_overlay], coordinates: [1, 8]},
+    {uv: %i[right_arm_back right_arm_overlay_back], coordinates: [32, 8]},
+    {uv: %i[left_arm left_arm_overlay], coordinates: [12, 8]},
+    {uv: %i[left_arm_back left_arm_overlay_back], coordinates: [21, 8]}
   ]
 
   FRONTBACK_MODEL_TO_UV = {
