741e79af249aac414741abae241d97e765771f47
Moxvallix Mox <moxvallix@gmail.com>
2024-10-21 23:16:04 +1030

    feat(i18n): more accessibility tweaks for the gallery

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -5,15 +5,15 @@ module UserHelper
   end
 
   def profile_link(user)
-    link_to user.to_path do
-      content = tag.span user.display_name, class: "underline"
+    link_to user.to_path, class: "underline" do
+      content = tag.span user.display_name
       if (badge = user.featured_badge).present?
         content += " "
         content += tag.img src: badge.url, alt: badge.name, title: badge.name, class: "inline w-4 border rounded-full box-content border-ncs-yellow-500 bg-ncs-yellow-500"
       end
       if user.role?
         content += " "
-        content += tag.img src: "/ncsassets/img/medals/#{user.role}.png", alt: user.role.titleize, title: user.role.titleize, class: "inline"
+        content += tag.img src: "/ncsassets/img/medals/#{user.role}.png", alt: user.role.titleize, title: "Site " + user.role.titleize, class: "inline"
       end
       content
     end
