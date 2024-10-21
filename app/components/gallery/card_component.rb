f18a1fbca1c9b2e8b5561a07887c618a62382d16
Moxvallix Mox <moxvallix@gmail.com>
2024-10-21 12:05:21 +1030

    feat(i18n): improve card links

--- app/components/gallery/card_component.rb
+++ app/components/gallery/card_component.rb
@@ -6,7 +6,8 @@ class Gallery::CardComponent < ViewComponent::Base
   renders_one :preview
   renders_one :overlay
 
-  def initialize(url: "")
+  def initialize(url: "", title: "")
     @url = url
+    @title = title
   end
 end
