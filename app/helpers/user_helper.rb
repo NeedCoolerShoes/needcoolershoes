32021dfa962519b8de26a5f67d4cb36e4e4a506f
Moxvallix <moxvallix@gmail.com>
2024-10-22 12:21:17 +1030

    feat(a11y): tweaks to gallery such that there are no more WAVE warnings

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -5,17 +5,20 @@ module UserHelper
   end
 
   def profile_link(user)
-    link_to user.to_path, class: "underline" do
-      content = tag.span user.display_name
+    tag.span class: "pointer-events-auto" do
+      content = ""
+
       if (badge = user.featured_badge).present?
         content += " "
-        content += tag.img src: badge.url, alt: badge.name, title: badge.name, class: "inline w-4 border rounded-full box-content border-ncs-yellow-500 bg-ncs-yellow-500"
+        content += tag.img src: badge.url, alt: "Badge icon", title: badge.name, class: "inline w-4 border rounded-full box-content border-ncs-yellow-500 bg-ncs-yellow-500"
       end
+
       if user.role?
         content += " "
-        content += tag.img src: "/ncsassets/img/medals/#{user.role}.png", alt: user.role.titleize, title: "Site " + user.role.titleize, class: "inline"
+        content += tag.img src: "/ncsassets/img/medals/#{user.role}.png", alt: "Role icon", title: "Site " + user.role.titleize, class: "inline"
       end
-      content
+
+      link_to(user.display_name, user.to_path, class: "underline") + content.html_safe
     end
   end
 end
