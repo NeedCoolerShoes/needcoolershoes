7ebd51d0066b7c35d0e8ed67f915003945f60e6c
Moxvallix Mox <moxvallix@gmail.com>
2024-11-11 17:31:21 +1030

    fix: try to fix cached assets middleware

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 require "active_support/core_ext/integer/time"
-require "middleware/cached_assets_middleware"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -23,7 +22,9 @@ Rails.application.configure do
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   config.public_file_server.enabled = true
-  config.middleware.insert_before ::ActionDispatch::Static, Middleware::CachedAssetsMiddleware
+
+  require "middleware/cached_assets_middleware"
+  config.middleware.insert_before ActionDispatch::Static, CachedAssetsMiddleware
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
