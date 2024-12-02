60bc5fed3fb4ea7e76449316ba7e3eab689e1e89
Moxvallix Mox <moxvallix@gmail.com>
2024-12-02 18:18:37 +1030

    fix: fix middleware

--- lib/middleware/cached_assets_middleware.rb
+++ lib/middleware/cached_assets_middleware.rb
@@ -1,16 +1,18 @@
-class CachedAssetsMiddleware
-  CACHED_PATH = "/assets"
-  CACHE_CONTROL = "public, max-age=30672000, immutable"
+module Middleware
+  class CachedAssetsMiddleware
+    CACHED_PATH = "/assets"
+    CACHE_CONTROL = "public, max-age=30672000, immutable"
 
-  def initialize(app)
-    @app = app
-  end
-
-  def call(env)
-    status, headers, body = @app.call(env)
-    if env["PATH_INFO"].to_s.start_with?(CACHED_PATH)
-      headers = headers.merge({"Cache-Control" => CACHE_CONTROL})
+    def initialize(app)
+      @app = app
     end
-    [status, headers, body]
+
+    def call(env)
+      status, headers, body = @app.call(env)
+      if env["PATH_INFO"].to_s.start_with?(CACHED_PATH)
+        headers = headers.merge({"Cache-Control" => CACHE_CONTROL})
+      end
+      [status, headers, body]
+    end
   end
 end
