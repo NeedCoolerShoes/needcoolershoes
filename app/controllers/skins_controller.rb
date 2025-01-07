e6b64ce3d1e471c5f1959e67159e7598401b7bde
Moxvallix <moxvallix@gmail.com>
2025-01-07 17:05:33 +1030

    feat(gallery): use skins without pagination for skin gallery filters

--- app/controllers/skins_controller.rb
+++ app/controllers/skins_controller.rb
@@ -30,6 +30,7 @@ class SkinsController < ApplicationController
     params[:page].to_i > 0 ? nil : params[:page] = 1
     items = (gallery_params[:items] || 24).to_i.clamp(1, 50)
     @pagy, @skins = pagy(skins, items: items)
+    @skins_all = skins
     index_meta_config
   rescue Pagy::OverflowError
     not_found_error
