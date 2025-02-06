bb8d6955f81ad4a63156a9d05b61ec5fba2d06d5
Moxvallix Mox <moxvallix@gmail.com>
2025-02-06 17:46:25 +1030

    Revert "feat(ui): add markdown editor for descriptions"
    This reverts commit 64382d85c4b932b07bd1e09e8989f5b86e38e8dc.

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,12 +1,9 @@
 import { Application } from "@hotwired/stimulus"
-import { MarksmithController } from '@avo-hq/marksmith'
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-
-application.register('marksmith', MarksmithController)
 
 export { application }
