fab5b16c914ab876547456506692f44dab846a1a
Moxvallix <dev@moxvallix.com>
2025-04-18 21:11:01 +0930

    chore: CORS allow all origins to access the gallery

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,18 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  if Rails.env.development?
-    allow do
-      origins "*"
-
-      resource "*",
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
-  end
-
   allow do
-    origins "beta.needcoolershoes.com"
+    origins "*"
 
-    resource "*",
+    resource "/gallery/*",
       headers: :any,
       methods: [:get, :options, :head]
   end
