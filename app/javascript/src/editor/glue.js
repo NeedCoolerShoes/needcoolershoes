3fc5eace2e20b5e4a0daeffbeb38ae07081b17fb
Moxvallix Mox <moxvallix@gmail.com>
2025-04-24 18:41:05 +0930

    feat(ux): add small screen warning

--- app/javascript/src/editor/glue.js
+++ app/javascript/src/editor/glue.js
@@ -4,3 +4,19 @@ document.getElementById("cancel-button").addEventListener("click", () => {
   const ui = document.getElementById("ncrs-ui");
   ui.exportModal.hide();
 });
+
+const warning = document.getElementById("warning");
+const acceptWarning = document.getElementById("accept-warning-button");
+
+if (window.matchMedia("(max-width: 800px)").matches) {
+  if (!localStorage.getItem("ncrs-ignore-warning")) {
+    warning.classList.remove("hidden");
+    warning.classList.add("flex");
+  }
+}
+
+acceptWarning.addEventListener("click", () => {
+  warning.classList.add('hidden');
+  warning.classList.remove('flex');
+  localStorage.setItem('ncrs-ignore-warning', true);
+});
