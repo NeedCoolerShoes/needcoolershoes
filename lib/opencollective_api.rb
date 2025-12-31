8fdae772c30386d68bb9136f125c3bf7016848a2
Moxvallix <dev@moxvallix.com>
2026-01-01 07:10:18 +1030

    feat(donations): implement donation bar yearly reset

--- lib/opencollective_api.rb
+++ lib/opencollective_api.rb
@@ -7,23 +7,24 @@ class OpencollectiveApi
   
   CACHE_TIME = 5.minutes
 
-  @@last_cached_at = Time.at(0)
-  @@cache = {balance: 0, goal: 0, name: "undefined", currency: "USD"}
+  @@cache = {balance: 0, goal: 0, name: "undefined", currency: "USD", cached_at: Time.at(0)}
 
   def self.get_goal_data
     if !COLLECTIVE_GOAL_ID || !COLLECTIVE_TOKEN || !COLLECTIVE_SLUG
       return @@cache
     end
 
-    if Time.current < (@@last_cached_at + CACHE_TIME)
+    if Time.current < (@@cache[:cached_at] + CACHE_TIME)
       return @@cache
     end
 
-    query = "query {collective(slug:\"#{COLLECTIVE_SLUG}\") {settings, stats {contributionsAmountTimeSeries(timeUnit: YEAR) {nodes {amount {valueInCents, currency}, label}}}}}"
+    start_time = Time.now.utc.at_beginning_of_year.iso8601
+
+    query = "query {collective(slug:\"#{COLLECTIVE_SLUG}\") {settings, stats {contributionsAmountTimeSeries(timeUnit: YEAR, dateFrom: \"#{start_time}\") {nodes {amount {valueInCents, currency}, label}}}}}"
     response = HTTP.headers("Personal-Token": COLLECTIVE_TOKEN).post(ENDPOINT, json: {query: query})
     data = JSON.parse(response.to_s)
 
-    goals = data.dig("data", "collective", "settings", "goals")
+    goals = data.dig("data", "collective", "settings", "goals") || []
     goal = goals.find {|g| g["key"] == COLLECTIVE_GOAL_ID }
 
     return @@cache unless goal.present?
@@ -39,12 +40,12 @@ class OpencollectiveApi
       goal: goal["amount"],
       name: goal["title"],
       currency: currency,
+      cached_at: Time.current
     }
-    @@last_cached_at = Time.current
     
     @@cache
   rescue
-    {balance: 0, goal: 0, name: "undefined", currency: "USD"}
+    {balance: 0, goal: 0, name: "undefined", currency: "USD", Time.current}
   end
 
   def self.donation_url
