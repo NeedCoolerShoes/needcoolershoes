a2f0f55cd99b9ba096b5e202546ed7b08ba120f7
Moxvallix Mox <moxvallix@gmail.com>
2026-04-01 18:22:13 +1030

    Revert "the button related commits"
    This reverts commit 2a100f671688597de6186ee7d68faae193cd722d.

--- config/routes.rb
+++ config/routes.rb
@@ -134,8 +134,6 @@ Rails.application.routes.draw do
     get "microsoft-identity-association", to: "site#microsoft_identity_association"
   end
 
-  post "button", as: "button", to: "site#button"
-
   # Mounts
   mount RailsAdmin::Engine => "/admin", :as => "rails_admin"
 end
